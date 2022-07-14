a11d9e4ae3d7f5625f4db6efa475785162714a27
csomtoo <chukwurahb@gmail.com>
2022-07-14 19:58:16 -0400

    first commit

---
+++ js/commands.js
@@ -0,0 +1,47 @@
+
+let password = "thegoat";
+
+whois = [
+"Hi im Da Vinci - an intelligent home automation system",
+  "<br>"
+];
+
+whoami = [
+  "IP address: 234:124:142",
+];
+
+
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+
+help = [
+  '<span class="command">whois</span>          Who is Da Vinci?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">login</span>          Welcome back',
+  '<span class="command">signup</span>         Only if your worthy',
+  '<span class="command">mic</span>            To lazy to type?',
+  '<span class="command">gui</span>            Open advanced gui',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Da Vinci Jaegerjaquez. Reiatsu is Life!!!.</span>',
+
+"  ____    _     __     _____ _   _  ____ ___ ",
+"|  _  \\ /   \\  \\\\   / /_ _|\\ | |/ ___|_ _|",
+"| | | |/ _ \\  \\\\ / / | || \\| | |    | | ",
+"| |_| / ___ \    \ V /  | || |\  | |___ | | ",
+"|____/_/  \\_\\   \\_/  |___|_|\\_|\\____|___|",
+
+
+  '<span class="color2">Welcome, be warned I am sentient.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
